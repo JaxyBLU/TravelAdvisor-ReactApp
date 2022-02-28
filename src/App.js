f59fcfea656889aa31b2f9177defb11e888da352
JaxyBLU <tukebaike123@gmail.com>
2022-02-28 16:08:17 +0400

    Added Header, Map, getPlacesData components and fixed Google Maps component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,41 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { CssBaseline, Grid } from '@material-ui/core';
 
+import { getPlacesData } from './api';
 import Header from './components/Header/Header';
 import List from './components/List/List';
 import Map from './components/Map/Map';
 import PlaceDetails from './components/PlaceDetails/PlaceDetails';
 
 export default function App () {
+    const[places, setPlaces] = useState([]);
+    const[coordinates, setCoordinates] = useState({});
+    const[bounds, setBounds] = useState(null);
+
+    useEffect(()=>{
+        getPlacesData()
+            .then((data)=>{
+                console.log(data);
+                setPlaces(data);
+            })
+    }, []);
+
     return(
-        <div>
+        <>
             <CssBaseline />
             <Header />
-            </div>
+            <Grid container spacing={3} style={{width: '100%'}}>
+                <Grid item xs={12} md={4}>
+                    <List />
+                </Grid>
+                <Grid item xs={12} md={8}>
+                    <Map
+                        setCoordinates={setCoordinates}
+                        setBounds={setBounds}
+                        coordinates={coordinates}
+                    />
+                </Grid>
+            </Grid>
+        </>
     );
 }
--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,7 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {
+  CircularProgress,
+  Grid,
+  Typography,
+  InputLabel,
+  MenuItem,
+  FormControl,
+  Select,
+} from '@material-ui/core';
+import PlaceDetails from '../PlaceDetails/PlaceDetails';
+import useStyles from './styles';
 
-export default function List () {
-    return(
-        <h1>List</h1>
-    );
+export default function List() {
+  const classes = useStyles();
+  const [type, setType] = useState('restaurants');
+  const [rating, setRating] = useState('');
+
+  const places = [
+    { name: 'Best Place' },
+    { name: 'Best Beer' },
+    { name: 'Best Steak' },
+    { name: 'Best Rooftop Bar' },
+    { name: 'Best Japanese Restaurant' },
+    { name: 'Best Sushi' },
+    { name: 'Best Cinema' },
+  ];
+
+  return (
+    <div className={classes.container}>
+      <Typography variant='h4'>
+        Restaurants, Hotels & Attractions around you
+      </Typography>
+      <FormControl className={classes.formControl}>
+        <InputLabel>Type</InputLabel>
+        <Select value={type} onChange={(e) => setType(e.target.value)}>
+          <MenuItem value='restaurants'>Restaurants</MenuItem>
+          <MenuItem value='hotels'>Hotels</MenuItem>
+          <MenuItem value='attractions'>Attractions</MenuItem>
+        </Select>
+      </FormControl>
+
+      <FormControl className={classes.formControl}>
+        <InputLabel>Rating</InputLabel>
+        <Select value={rating} onChange={(e) => setRating(e.target.value)}>
+          <MenuItem value={0}>All</MenuItem>
+          <MenuItem value={1}>Above 3.0</MenuItem>
+          <MenuItem value={2}>Above 4.0</MenuItem>
+          <MenuItem value={3}>Above 4.5</MenuItem>
+        </Select>
+      </FormControl>
+      <Grid container spacing={3} className={classes.list}>
+        {places?.map((place, index) => (
+          <Grid item key={index} xs={12}>
+            <PlaceDetails place={place} />
+          </Grid>
+        ))}
+      </Grid>
+    </div>
+  );
 }
