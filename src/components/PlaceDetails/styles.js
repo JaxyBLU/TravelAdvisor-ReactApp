5dc8096472eca1bd8f22ce9e195a1031cfc9b074
JaxyBLU <tukebaike123@gmail.com>
2022-03-04 23:12:20 +0400

    Today I added each List Item the price range value and ranking value and also the awards, image of the item and also cuisine type of the restarurant as well, and also fixed the issue with the intial launch of the page

--- src/components/PlaceDetails/styles.js
+++ src/components/PlaceDetails/styles.js
@@ -1,5 +1,13 @@
-import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 
-export default function PlaceDetails({ place }) {
-  return <h1>{place.name}</h1>;
-}
+export default makeStyles(() => ({
+  chip: {
+    margin: '5px 5px 5px 0',
+  },
+  subtitle: {
+    display: 'flex', alignItems: 'center', justifyContent: 'space-between', marginTop: '10px',
+  },
+  spacing: {
+    display: 'flex', alignItems: 'center', justifyContent: 'space-between',
+  },
+}));
